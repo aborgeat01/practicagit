34e3888a1ff3b575fbc69077a116bab481563d76
unknown <aborgeat@unlam.edu.ar>
2021-04-05 21:56:45 -0300

    zz

--- src/ar/edu/unlam/git/Menu.java
+++ src/ar/edu/unlam/git/Menu.java
@@ -3,7 +3,7 @@ package ar.edu.unlam.git;
 public class Menu {
 
 	public static void main(String[] args) {
-    System.out.println("hola mundo");
+    System.out.println("hola");
 	int i = 1;
 	System.out.println("hola Mundo" + i);
 	}
